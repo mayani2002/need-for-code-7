a28947c00c7efccf6844f56b9dafd9cb961fab0c
mayani <arti.agni01@gmail.com>
2022-07-26 17:38:37 +0530

    modify UI

--- client/src/layouts/dashboard/NavConfig.js
+++ client/src/layouts/dashboard/NavConfig.js
@@ -20,27 +20,7 @@ const navConfig = [
     title: 'product',
     path: '/dashboard/products',
     icon: getIcon('eva:shopping-bag-fill'),
-  },
-  {
-    title: 'blog',
-    path: '/dashboard/blog',
-    icon: getIcon('eva:file-text-fill'),
-  },
-  {
-    title: 'login',
-    path: '/login',
-    icon: getIcon('eva:lock-fill'),
-  },
-  {
-    title: 'register',
-    path: '/register',
-    icon: getIcon('eva:person-add-fill'),
-  },
-  {
-    title: 'Not found',
-    path: '/404',
-    icon: getIcon('eva:alert-triangle-fill'),
-  },
+  }
 ];
 
 export default navConfig;
